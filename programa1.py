d46cd97fe60e6758e88e5340a6f7cda8782f20b7
Israel Gutierrez <israwuarrior@gmail.com>
2021-07-01 14:50:19 -0500

    arranco mi proyecto real en html

---
+++ programa1.py
@@ -0,0 +1,17 @@
+# Este programa suma dos numeros
+
+a = 2
+b = 5
+
+suma = a + b
+
+print('La suma es: ', suma)
+
+# Este programa resta dos numeros
+
+a = 2
+b = 5
+
+resta = a - b
+
+print('La resta es: ', resta)
